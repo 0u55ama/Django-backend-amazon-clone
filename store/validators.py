160aac0de55fef1e1a0419309b4a2e616f80b0c9
Oussama <oussamaait2001@gmail.com>
2023-08-29 13:43:17 +0100

    adding log to the / page

---
+++ store/validators.py
@@ -0,0 +1,6 @@
+from django.core.validators import ValidationError
+
+def validate_file_size(file):
+    max_size_kb = 200
+    if file.size > max_size_kb * 1024:
+        raise ValidationError(f'File size cannot be larger than {max_size_kb}KB.')
